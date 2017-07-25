49c75dfa3d473ffc8d85db123fba6d6a14489e62
Jake Lever <jlever@bcgsc.ca>
2017-07-25 13:28:07 -0700

    Add test for generating docstrings

---
+++ tests/test_docstrings.py
@@ -0,0 +1,59 @@
+
+import sys
+import os
+import importlib
+import docstringtest
+
+def addDataDirToPath():
+	thisDir = os.path.dirname(os.path.realpath(__file__))
+	dataDir = os.path.join(thisDir,'data')
+	sys.path.append(dataDir)
+	return dataDir
+
+def test_docstring_constructor():
+	addDataDirToPath()
+	mod = importlib.import_module('testclass_good')
+
+	expected = [':param self: description',
+		':param varA: description',
+		':param varB: description',
+		':param varC: description',
+		':type self: type description',
+		':type varA: type description',
+		':type varB: type description',
+		':type varC: type description']
+	docstring = docstringtest.generateDocstring(mod.TestClass.__init__)
+
+	assert docstring == "\n".join(expected)
+
+
+def test_docstring_method():
+	addDataDirToPath()
+	mod = importlib.import_module('testclass_good')
+
+	expected = [':param self: description',
+		':param varA: description',
+		':param varB: description',
+		':type self: type description',
+		':type varA: type description',
+		':type varB: type description']
+	docstring = docstringtest.generateDocstring(mod.TestClass.basicMethod)
+
+	assert docstring == "\n".join(expected)
+
+def test_docstring_withReturn():
+	addDataDirToPath()
+	mod = importlib.import_module('testclass_good')
+
+	expected = [':param self: description',
+		':param varA: description',
+		':param varB: description',
+		':type self: type description',
+		':type varA: type description',
+		':type varB: type description',
+		':return: return description',
+		':rtype: the return type description']
+	docstring = docstringtest.generateDocstring(mod.TestClass.basicMethodWithReturn)
+
+	assert docstring == "\n".join(expected)
+
