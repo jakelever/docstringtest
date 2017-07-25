d0db1f8d265cb1070d80ed4da45509e8200c0221
Jake Lever <jlever@bcgsc.ca>
2017-07-25 13:28:27 -0700

    Add code for generating docstrings

--- docstringtest/__init__.py
+++ docstringtest/__init__.py
@@ -1,6 +1,27 @@
 
 import inspect
 
+def codeReturnsSomething(m):
+	sourceCode = inspect.getsource(m).split('\n')
+	for line in sourceCode:
+		tokens = line.strip().split()
+		if len(tokens) >= 2 and tokens[0] == 'return':
+			return True
+	return False
+
+def generateDocstring(m):
+	methodArgs = m.__code__.co_varnames[:m.__code__.co_argcount]
+	
+	params = [ ":param %s: description" % methodArg for methodArg in methodArgs ]
+	types = [ ":type %s: type description" % methodArg for methodArg in methodArgs ]
+	returns = []
+	if codeReturnsSomething(m):
+		returns = [":return: return description",":rtype: the return type description"]
+
+	txt = "\n".join(params + types + returns)
+
+	return txt
+
 def processMethod(m):
 	methodArgs = m.__code__.co_varnames[:m.__code__.co_argcount]
 	
