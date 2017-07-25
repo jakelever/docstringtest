466725939e5e2649ee4870339782585411610368
Jake Lever <jlever@bcgsc.ca>
2017-07-25 13:47:32 -0700

    Some variable rename

--- docstringtest/__init__.py
+++ docstringtest/__init__.py
@@ -1,21 +1,21 @@
 
 import inspect
 
-def codeReturnsSomething(m):
-	sourceCode = inspect.getsource(m).split('\n')
+def codeReturnsSomething(func):
+	sourceCode = inspect.getsource(func).split('\n')
 	for line in sourceCode:
 		tokens = line.strip().split()
 		if len(tokens) >= 2 and (tokens[0] == 'return' or tokens[0] == 'yield'):
 			return True
 	return False
 
-def generateDocstring(m):
-	methodArgs = m.__code__.co_varnames[:m.__code__.co_argcount]
+def generateDocstring(func):
+	methodArgs = func.__code__.co_varnames[:func.__code__.co_argcount]
 	
 	params = [ ":param %s: description" % methodArg for methodArg in methodArgs ]
 	types = [ ":type %s: type description" % methodArg for methodArg in methodArgs ]
 	returns = []
-	if codeReturnsSomething(m):
+	if codeReturnsSomething(func):
 		returns = [":return: return description",":rtype: the return type description"]
 
 	txt = "\n".join(params + types + returns)
@@ -24,7 +24,7 @@ def generateDocstring(m):
 
 def processFunction(func):
 	methodArgs = func.__code__.co_varnames[:func.__code__.co_argcount]
-	
+
 
 def processClass(c):
 	for name,obj in inspect.getmembers(c):
