ae939fb96b449ca3381560d5222f3a84b4a9989b
Jake Lever <jlever@bcgsc.ca>
2017-07-25 11:56:44 -0700

    Fix Py3 print

--- docstringtest/__init__.py
+++ docstringtest/__init__.py
@@ -9,11 +9,11 @@ def processClass(c):
 	for name,obj in inspect.getmembers(c):
 		#print name,obj
 		if inspect.ismethod(obj):
-			print name
+			print(name)
 			processMethod(obj)
 
 def processModule(m):
 	for name,obj in inspect.getmembers(m):
 		if inspect.isclass(obj):
-			print "#",name
+			print("#",name)
 			processClass(obj)
