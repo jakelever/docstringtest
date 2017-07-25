129f356f3efa9ab60a3171af27b8ca6d21170569
Jake Lever <jlever@bcgsc.ca>
2017-07-25 11:14:38 -0700

    Add example test files

---
+++ tests/data/testclass_good.py
@@ -0,0 +1,67 @@
+
+class TestClass:
+	def __init__(self, varA, varB, varC):
+		"""
+		Some basic description
+
+		:param self: description
+		:param varA: description
+		:param varB: description
+		:param varC: description
+		:type self: type description
+		:type varA: type description
+		:type varB: type description
+		:type varC: type description
+		"""
+		pass
+
+	def _shouldBeIgnore(self, varA, varB):
+		pass
+
+	def staticMethod(varA):
+		"""
+		Some basic description
+
+		:param varA: description
+		:type varA: type description
+		"""
+		pass
+	
+	def staticMethodWithReturn(varA):
+		"""
+		Some basic description
+
+		:param varA: description
+		:type varA: type description
+		:return: return description
+		:rtype: the return type description
+		"""
+		return 1
+
+	def basicMethod(self,varA,varB):
+		"""
+		Some basic description
+
+		:param self: description
+		:param varA: description
+		:param varB: description
+		:type self: type description
+		:type varA: type description
+		:type varB: type description
+		"""
+		pass
+
+	def basicMethodWithReturn(self,varA,varB):
+		"""
+		Some basic description
+
+		:param self: description
+		:param varA: description
+		:param varB: description
+		:type self: type description
+		:type varA: type description
+		:type varB: type description
+		:return: return description
+		:rtype: the return type description
+		"""
+		return 1
---
+++ tests/data/testfunctions_good.py
@@ -0,0 +1,34 @@
+
+def testfunction(varA, varB):
+	"""
+	Some basic description
+
+	:param varA: description
+	:param varB: description
+	:type varA: type description
+	:type varB: type description
+	"""
+	pass
+
+def testfunctionWithReturn(varA, varB, varC):
+	"""
+	Some basic description
+
+	:param varA: description
+	:param varB: description
+	:param varC: description
+	:type self: type description
+	:type varA: type description
+	:type varB: type description
+	:type varC: type description
+	:return: return description
+	:rtype: the return type description
+	"""
+	return 1
+
+def _shouldBeSkipped(varA, varB):
+	pass
+
+def _shouldBeSkippedWithReturn(varA, varB):
+	return 1
+
