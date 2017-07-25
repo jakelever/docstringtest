1ecd936a1a28ea01031b1d51ed72ee96b23789bc
Jake Lever <jlever@bcgsc.ca>
2017-07-25 11:46:04 -0700

    Add passing tests

---
+++ tests/test_class_good.py
@@ -0,0 +1,25 @@
+
+import sys
+import os
+import importlib
+import docstringtest
+
+def addDataDirToPath():
+	thisDir = os.path.dirname(os.path.realpath(__file__))
+	dataDir = os.path.join(thisDir,'data')
+	sys.path.append(dataDir)
+	return dataDir
+
+def test_module_good():
+	addDataDirToPath()
+	mod = importlib.import_module('testclass_good')
+
+	# Shouldn't throw any errors
+	docstringtest.processModule(mod)
+
+def test_class_good():
+	addDataDirToPath()
+	mod = importlib.import_module('testclass_good')
+
+	# Shouldn't throw any errors
+	docstringtest.processClass(mod.TestClass)
