6f9be3fab212ac76fa23d33c1dd551a102ecbb11
Jake Lever <jlever@bcgsc.ca>
2017-07-25 16:11:34 -0700

    More testing

--- tests/test_class_good.py
+++ tests/test_class_good.py
@@ -13,7 +13,43 @@ def test_class_good():
 	# Shouldn't throw any errors
 	docstringtest.processClass(docstringtest.examples.testclass_good.ExampleGoodClass)
 
-def test_function_good_constructor():
-	# Shouldn't throw any errors
+def test_function_good___init__():
+	# Should not throw any errors
 	docstringtest.processFunction(docstringtest.examples.testclass_good.ExampleGoodClass.__init__)
+
+def test_function_good__shouldBeIgnored():
+	# Should not throw any errors
+	docstringtest.processFunction(docstringtest.examples.testclass_good.ExampleGoodClass._shouldBeIgnored)
+
+def test_function_good_staticMethod():
+	# Should not throw any errors
+	docstringtest.processFunction(docstringtest.examples.testclass_good.ExampleGoodClass.staticMethod)
+
+def test_function_good_staticMethodWithReturn():
+	# Should not throw any errors
+	docstringtest.processFunction(docstringtest.examples.testclass_good.ExampleGoodClass.staticMethodWithReturn)
+
+def test_function_good_basicMethod():
+	# Should not throw any errors
+	docstringtest.processFunction(docstringtest.examples.testclass_good.ExampleGoodClass.basicMethod)
+
+def test_function_good_basicMethodWithReturn():
+	# Should not throw any errors
+	docstringtest.processFunction(docstringtest.examples.testclass_good.ExampleGoodClass.basicMethodWithReturn)
+
+def test_function_good_basicMethodWithYield():
+	# Should not throw any errors
+	docstringtest.processFunction(docstringtest.examples.testclass_good.ExampleGoodClass.basicMethodWithYield)
+
+def test_function_good_staticMethodNoVariables():
+	# Should not throw any errors
+	docstringtest.processFunction(docstringtest.examples.testclass_good.ExampleGoodClass.staticMethodNoVariables)
+
+def test_function_good_staticMethodNoVariablesWithReturn():
+	# Should not throw any errors
+	docstringtest.processFunction(docstringtest.examples.testclass_good.ExampleGoodClass.staticMethodNoVariablesWithReturn)
+
+def test_function_good_staticMethodNoVariablesWithYield():
+	# Should not throw any errors
+	docstringtest.processFunction(docstringtest.examples.testclass_good.ExampleGoodClass.staticMethodNoVariablesWithYield)
 
