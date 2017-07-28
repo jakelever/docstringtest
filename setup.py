c30ffe5b4a739d9af107a5fbe00f4f77ef20cfda
Jake Lever <jlever@bcgsc.ca>
2017-07-27 20:10:40 -0700

    Update to 0.1.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(name='docstringtest',
 
-	version='0.1.0',
+	version='0.1.1',
 
 	description='Procedure to check for valid Sphinx ReST docstrings',
 	long_description=long_description,
