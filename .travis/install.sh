c830ed45befd146f5d68c24d3bb83107aa1ca211
Jake Lever <jlever@bcgsc.ca>
2017-08-05 00:54:19 -0700

    Another go with python virtualenv

--- .travis/install.sh
+++ .travis/install.sh
@@ -11,23 +11,27 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	case "${TOXENV}" in
 	py27)
 		# Install some custom Python 3.2 requirements on OS X
-		#pyenv virtualenv 2.7.10 my-virtual-env-2.7.10
-		#pyenv activate my-virtual-env-2.7.10
-		brew upgrade python;
+		pyenv install 2.7.10
+		pyenv virtualenv 2.7.10 my-virtual-env-2.7.10
+		pyenv activate my-virtual-env-2.7.10
+#		brew upgrade python;
 #		pip install --upgrade virtualenv
 #		python -m venv venv;
 #		source venv/bin/activate;
-		pyenv virtualenv -p /usr/local/opt/python/libexec/bin/python2 myenv2
-		pyenv activate myenv2
+#		pyenv virtualenv -p /usr/local/opt/python/libexec/bin/python2 myenv2
+#		pyenv activate myenv2
 		;;
 	py36)
+		pyenv install 3.5.0
+		pyenv virtualenv 3.5.0 my-virtual-env-3.5.0
+		pyenv activate my-virtual-env-3.5.0
 		# Install some custom Python 3.3 requirements on OS X
-		brew install python3;
+#		brew install python3;
 #		pip3
 #		python3 -m venv venv;
 #		source venv/bin/activate;
-		pyenv virtualenv -p `which python3` myenv3
-		pyenv activate myenv3
+#		pyenv virtualenv -p `which python3` myenv3
+#		pyenv activate myenv3
 		;;
 	esac
 else
