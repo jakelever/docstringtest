c7a224623b10bc1a771937a74a3cf9e10fd71289
Jake Lever <jlever@bcgsc.ca>
2017-08-05 00:26:32 -0700

    More steps towards MacOS support

--- .travis/install.sh
+++ .travis/install.sh
@@ -13,13 +13,15 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 		# Install some custom Python 3.2 requirements on OS X
 		#pyenv virtualenv 2.7.10 my-virtual-env-2.7.10
 		#pyenv activate my-virtual-env-2.7.10
-		brew install python;
+		brew upgrade python;
+		pip install --upgrade virtualenv
 		python -m venv venv;
 		source venv/bin/activate;
 		;;
 	py36)
 		# Install some custom Python 3.3 requirements on OS X
 		brew install python3;
+		pip3
 		python3 -m venv venv;
 		source venv/bin/activate;
 		;;
