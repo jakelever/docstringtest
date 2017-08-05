39377ebfabf7d4fcf54b82adcf92ca4069e170ba
Jake Lever <jlever@bcgsc.ca>
2017-08-05 01:18:43 -0700

    Use virtualenv for MacOS"

--- .travis/install.sh
+++ .travis/install.sh
@@ -8,34 +8,38 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	# e.g. brew install pyenv-virtualenv
 
 	brew update
-	brew install pyenv-virtualenv
-	eval "$(pyenv init -)"
-	eval "$(pyenv virtualenv-init -)"
+#	brew install pyenv-virtualenv
+#	eval "$(pyenv init -)"
+#	eval "$(pyenv virtualenv-init -)"
 
 	case "${TOXENV}" in
 	py27)
 		# Install some custom Python 3.2 requirements on OS X
-		pyenv install 2.7.10
-		pyenv virtualenv 2.7.10 my-virtual-env-2.7.10
-		pyenv activate my-virtual-env-2.7.10
-#		brew upgrade python;
+#		pyenv install 2.7.10
+#		pyenv virtualenv 2.7.10 my-virtual-env-2.7.10
+#		pyenv activate my-virtual-env-2.7.10
+		brew upgrade python;
 #		pip install --upgrade virtualenv
 #		python -m venv venv;
 #		source venv/bin/activate;
 #		pyenv virtualenv -p /usr/local/opt/python/libexec/bin/python2 myenv2
 #		pyenv activate myenv2
+		virtualenv env2 -p python2
+		source env2/bin/activate
 		;;
 	py36)
-		pyenv install 3.5.0
-		pyenv virtualenv 3.5.0 my-virtual-env-3.5.0
-		pyenv activate my-virtual-env-3.5.0
+#		pyenv install 3.5.0
+#		pyenv virtualenv 3.5.0 my-virtual-env-3.5.0
+#		pyenv activate my-virtual-env-3.5.0
 		# Install some custom Python 3.3 requirements on OS X
-#		brew install python3;
+		brew install python3;
 #		pip3
 #		python3 -m venv venv;
 #		source venv/bin/activate;
 #		pyenv virtualenv -p `which python3` myenv3
 #		pyenv activate myenv3
+		virtualenv env3 -p python3
+		source env3/bin/activate
 		;;
 	esac
 else
