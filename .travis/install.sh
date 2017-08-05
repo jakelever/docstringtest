9bcd4ea8feb97d2ece322eb9ea3eb5379eeb2253
Jake Lever <jlever@bcgsc.ca>
2017-08-05 02:51:08 -0700

    Cleanup of Travis-related code

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,45 +1,21 @@
 #!/bin/bash
 set -exo pipefail
 
-echo "travis_fold:start:SCRIPT folding starts"
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
 	# Install some custom requirements on OS X
-	# e.g. brew install pyenv-virtualenv
-
 	brew update
-#	brew install pyenv-virtualenv
-#	eval "$(pyenv init -)"
-#	eval "$(pyenv virtualenv-init -)"
 
 	case "${TOXENV}" in
 	py27)
 		# Install some custom Python 3.2 requirements on OS X
-#		pyenv install 2.7.10
-#		pyenv virtualenv 2.7.10 my-virtual-env-2.7.10
-#		pyenv activate my-virtual-env-2.7.10
 		brew upgrade python;
-#		pip install --upgrade virtualenv
-#		python -m venv venv;
-#		source venv/bin/activate;
-#		pyenv virtualenv -p /usr/local/opt/python/libexec/bin/python2 myenv2
-#		pyenv activate myenv2
 		pip2 install --upgrade virtualenv
 		virtualenv env2 -p python2
-		ls -l env2/bin
 		PS1=${PS1:=} source env2/bin/activate
 		;;
 	py36)
-#		pyenv install 3.5.0
-#		pyenv virtualenv 3.5.0 my-virtual-env-3.5.0
-#		pyenv activate my-virtual-env-3.5.0
-		# Install some custom Python 3.3 requirements on OS X
 		brew install python3;
-#		pip3
-#		python3 -m venv venv;
-#		source venv/bin/activate;
-#		pyenv virtualenv -p `which python3` myenv3
-#		pyenv activate myenv3
 		pip3 install --upgrade virtualenv
 		virtualenv env3 -p python3
 		PS1=${PS1:=} source env3/bin/activate
@@ -47,7 +23,6 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	esac
 else
 	# Install some custom requirements on Linux
-	done=1
+	echo
 fi
-echo "travis_fold:end:SCRIPT folding ends"
 
