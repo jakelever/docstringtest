b2cc2e0ee9feed2052a32b084b0aaa9f6c5f16df
Jake Lever <jlever@bcgsc.ca>
2017-08-13 16:06:59 -0700

    Increment version number

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(name='docstringtest',
 
-	version='0.2.0',
+	version='0.3.0',
 
 	description='Procedure to check for valid Sphinx ReST docstrings',
 	long_description=long_description,
